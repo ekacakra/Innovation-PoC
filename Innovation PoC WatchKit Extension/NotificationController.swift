752f20ead735a397cf9187f2aa83713e4e28ab9b
Ibnu Siena <ibnu.siena@photoninfotech.net>
2015-05-11 18:50:50 +0700

    Update Dynamic Notification UI

--- Innovation PoC WatchKit Extension/NotificationController.swift
+++ Innovation PoC WatchKit Extension/NotificationController.swift
@@ -12,6 +12,11 @@ import Foundation
 
 class NotificationController: WKUserNotificationInterfaceController {
 
+    @IBOutlet weak var dateText: WKInterfaceLabel!
+    @IBOutlet weak var dayText: WKInterfaceLabel!
+    @IBOutlet weak var timeText: WKInterfaceLabel!
+    @IBOutlet weak var placeText: WKInterfaceLabel!
+    @IBOutlet weak var message: WKInterfaceLabel!
     override init() {
         // Initialize variables here.
         super.init()
@@ -40,14 +45,25 @@ class NotificationController: WKUserNotificationInterfaceController {
     }
     */
     
-    /*
+    
     override func didReceiveRemoteNotification(remoteNotification: [NSObject : AnyObject], withCompletion completionHandler: ((WKUserNotificationInterfaceType) -> Void)) {
         // This method is called when a remote notification needs to be presented.
         // Implement it if you use a dynamic notification interface.
         // Populate your dynamic notification interface as quickly as possible.
         //
         // After populating your dynamic notification interface call the completion block.
+        
+        if remoteNotification.isEmpty == false {
+            if let aps = (remoteNotification["aps"] as? NSDictionary) {
+                dateText.setText(aps["date"] as? String)
+                dayText.setText(aps["day"] as? String)
+                timeText.setText(aps["time"] as? String)
+                placeText.setText(aps["place"] as? String)
+                message.setText(aps["note"] as? String)
+            }
+        }
+        
         completionHandler(.Custom)
     }
-    */
+    
 }
